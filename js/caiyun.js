891c24922cb639ae45ccb9cc8223658853e9b7ae
0326 <winniringy@gmail.com>
2015-04-26 23:58:18 +0800

    add baidumap

--- js/caiyun.js
+++ js/caiyun.js
@@ -65,7 +65,7 @@ $(document).ready(function() {
 				$(".section2").find(".intro").addClass("animated zoomIn");
 			}
 			if(index == 6){
-				$('.contact-con a').fadeIn(1000);
+				initMap();
 			}
 		},
 		onLeave: function(index, direction){
@@ -79,9 +79,31 @@ $(document).ready(function() {
 
 			}
 			if(index == 6){
-				$('.zanzhu-con a').fadeOut(1000);
+
 			}
 		}
 	});
+	//百度地图
+	function initMap (argument) {
+    var sContent = '<div style="margin:0;line-height:20px;padding:12px 6px;width:300px;">' +
+                    '<img id="mapImg" src="img/home/0.jpg" alt="" style="float:right;zoom:1;overflow:hidden;width:100px;height:100px;margin-left:3px;"/>' +
+                    '<p>联系人：陈小姐</p><p>电话：(010)59928888</p><p>地址：河北省承德市丰宁满族自治县大滩镇元山子村</p>' +
+                  '</div>';
+    var map = new BMap.Map("baiduMap");
+  	var point = new BMap.Point(116.011016,41.613199);
+  	var marker = new BMap.Marker(point);
+  	var infoWindow = new BMap.InfoWindow(sContent);  // 创建信息窗口对象
+  	infoWindow.width = 300;
+  	map.centerAndZoom(point, 8);
+  	map.addOverlay(marker);
+  	map.addControl(new BMap.NavigationControl());    
+  	map.addControl(new BMap.ScaleControl());    
+  	map.addControl(new BMap.OverviewMapControl());  
+	  marker.openInfoWindow(infoWindow);
+	  //图片加载完毕重绘infowindow
+	  document.getElementById('mapImg').onload = function (){
+		  infoWindow.redraw();   //防止在网速较慢，图片未加载时，生成的信息框高度比图片的总高度小，导致图片部分被隐藏
+	  }
+	}
 });
 
